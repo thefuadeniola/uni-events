f744773ca9408e1f86abcb6961e72fde7627d72f
Davies David <you@example.com>
2024-02-20 18:34:33 +0100

    Complete Event Form

--- components/shared/dropdown.tsx
+++ components/shared/dropdown.tsx
@@ -1,9 +1,10 @@
 'use client'
-import React, {startTransition, useState} from 'react'
+import React, {startTransition, useEffect, useState} from 'react'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { ICategory } from '@/lib/database/models/category.model'
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger, } from "@/components/ui/alert-dialog"
 import { Input } from '../ui/input'
+import { createCategory, getAllCategories } from '@/lib/actions/category.actions'
   
 
 type DropdownProps = {
@@ -15,7 +16,23 @@ const Dropdown = ({onChangeHandler, value} : DropdownProps) => {
 
   const [categories, setCategories] = useState<ICategory[]>([])
   const [newCategory, setNewCategory] = useState('')  
-  const handleAddCategory = () => {}
+  const handleAddCategory = () => {
+    createCategory({
+        categoryName: newCategory.trim()
+    })
+    .then((category) => {
+        setCategories((prevState) => [...prevState, category])
+    })
+  }
+  
+  useEffect(()=>{
+    const getCategories = async () => {
+        const categoryList = await getAllCategories()
+
+        categoryList && setCategories(categoryList as ICategory[])
+    }
+    getCategories();
+  }, [])
 
   return (
     <Select onValueChange={onChangeHandler} defaultValue={value}>
@@ -32,7 +49,7 @@ const Dropdown = ({onChangeHandler, value} : DropdownProps) => {
             }    
 
             <AlertDialog>
-            <AlertDialogTrigger className='p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500'>Open</AlertDialogTrigger>
+                <AlertDialogTrigger className='p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500'>Add New Category</AlertDialogTrigger>
                 <AlertDialogContent className='bg-white'>
                     <AlertDialogHeader>
                         <AlertDialogTitle>New Category</AlertDialogTitle>
--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -9,4 +9,6 @@ const CategorySchema = new Schema({
     name: { type: String, required: true, unique: true }
 })
 
-const category = models.Category || model('Category', CategorySchema)
+const Category = models.Category || model('Category', CategorySchema)
+
+export default Category
